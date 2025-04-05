5fb2fa073192fb2ce5d143370f54c66940aaade7
Bhavya <bbhojwani31@gmail.com>
2025-04-05 09:37:42 +0530

    Added component and pages folder and working on navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "home",
+        element: <Home />
+      },
+      {
+
+      }
+    ]
+  }
+])
+
 
 function App() {
-  
+
 
   return (
-    <>
-      
-    </>
+    <RouterProvider router={router}>
+      <Layout />
+    </RouterProvider>
   )
 }
 
---
+++ src/Components/Header/Avatar.jsx
@@ -0,0 +1,11 @@
+function Avatar() {
+  return (
+    <div className="avatar">
+      <div className="w-12 h-12 rounded-full object-cover">
+        <img className='rounded-full' src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+      </div>
+    </div>
+  );
+}
+
+export default Avatar;
